4068ecaf378c471e071849082b964553ea2a2ceb
Dan O'Meara <danomearawd@gmail.com>
2024-10-17 17:44:26 -0400

    UPDATE
    NEW ERD with no composite keys (weren't necessary)
    
    queries.sql - checks row count of tables and comments give row count of csvs
    
    schema.sql - no more composite keys, they were unnecessary.

--- queries.sql
+++ queries.sql
@@ -1,16 +1,36 @@
+--TEST IMPORTS - COUNTS SHOULD MATCH CSVS
+SELECT COUNT(*)
+FROM Departments --SHOULD BE 9
+
+SELECT COUNT(*)
+FROM Titles --SHOULD BE 331603
+
+SELECT COUNT(*)
+FROM Employees --SHOULD BE 300024
+
+SELECT COUNT(*)
+FROM Dept_emp --SHOULD BE 331603
+
+SELECT COUNT(*)
+FROM Dept_manager --SHOULD BE 24
+
+SELECT COUNT(*)
+FROM Salaries --SHOULD BE 300024
+
+
+
 --1. List the employee number, last name, first name, sex, and salary of each employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
-JOIN salaries s ON e.emp_no = s.emp_no
+JOIN salaries s ON e.emp_no = s.emp_no;
 
 --2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date LIKE '%1986'
+WHERE hire_date LIKE '%1986';
 
 --3. List the manager of each department along with their department number, department name,
 --employee number, last name, and first name.
-
 
 --4. List the department number for each employee along with that employee’s employee number,
 --last name, first name, and department name.
--- schema.sql
+++ schema.sql
@@ -1,18 +1,18 @@
--- data/departments.csv
+-- data/departments.csv 9 rows
 CREATE TABLE IF NOT EXISTS Departments (
-    dept_no CHAR(4) NOT NULL PRIMARY KEY,
-    dept_name VARCHAR(25) NOT NULL
+    dept_no CHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(25) NOT NULL UNIQUE
 );
 
--- data/titles.csv
+-- data/titles.csv 331603 rows
 CREATE TABLE IF NOT EXISTS Titles (
-    title_id CHAR(5) NOT NULL PRIMARY KEY,
-    title VARCHAR(25) NOT NULL
+    title_id CHAR(5) PRIMARY KEY,
+    title VARCHAR(25) NOT NULL UNIQUE
 );
 
--- data/employees.csv
+-- data/employees.csv 300024 rows
 CREATE TABLE IF NOT EXISTS Employees (
-    emp_no INT NOT NULL PRIMARY KEY,
+    emp_no INT PRIMARY KEY,
     emp_title_id CHAR(5) NOT NULL,
     birth_date VARCHAR(10) NOT NULL,
     first_name VARCHAR(25) NOT NULL,
@@ -22,27 +22,25 @@ CREATE TABLE IF NOT EXISTS Employees (
     FOREIGN KEY (emp_title_id) REFERENCES Titles (title_id)
 );
 
--- data/dept_emp.csv
+-- data/dept_emp.csv 331603 rows
 CREATE TABLE IF NOT EXISTS Dept_emp (
-    emp_no INT NOT NULL,
-    dept_no CHAR(4) NOT NULL,
-    PRIMARY KEY (emp_no, dept_no),
+    emp_no INT PRIMARY KEY,
+    dept_no CHAR(4),
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
 );
 
--- data/dept_manager.csv
+-- data/dept_manager.csv 24 rows
 CREATE TABLE IF NOT EXISTS Dept_manager (
-    dept_no CHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    PRIMARY KEY (dept_no, emp_no),
+    dept_no CHAR(4),
+    emp_no INT PRIMARY KEY,
     FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
 );
 
--- data/salaries.csv
+-- data/salaries.csv 300024 rows
 CREATE TABLE IF NOT EXISTS Salaries (
-    emp_no INT NOT NULL PRIMARY KEY,
+    emp_no INT PRIMARY KEY,
     salary INT NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
 );
