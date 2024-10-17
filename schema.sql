08fd07f6cc9434a18db89ad277de374febbc9cca
Dan O'Meara <danomearawd@gmail.com>
2024-10-17 01:41:41 -0400

    separated sql files

--- schema.sql
+++ schema.sql
@@ -1,18 +1,16 @@
-﻿--Data Engineering
-
---departments.csv
+-- data/departments.csv
 CREATE TABLE IF NOT EXISTS Departments (
     dept_no CHAR(4) NOT NULL PRIMARY KEY,
     dept_name VARCHAR(25) NOT NULL
 );
 
---titles.csv
+-- data/titles.csv
 CREATE TABLE IF NOT EXISTS Titles (
     title_id CHAR(5) NOT NULL PRIMARY KEY,
     title VARCHAR(25) NOT NULL
 );
 
---employees.csv
+-- data/employees.csv
 CREATE TABLE IF NOT EXISTS Employees (
     emp_no INT NOT NULL PRIMARY KEY,
     emp_title_id CHAR(5) NOT NULL,
@@ -24,7 +22,7 @@ CREATE TABLE IF NOT EXISTS Employees (
     FOREIGN KEY (emp_title_id) REFERENCES Titles (title_id)
 );
 
---dept_emp.csv
+-- data/dept_emp.csv
 CREATE TABLE IF NOT EXISTS Dept_emp (
     emp_no INT NOT NULL,
     dept_no CHAR(4) NOT NULL,
@@ -33,7 +31,7 @@ CREATE TABLE IF NOT EXISTS Dept_emp (
     FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
 );
 
---dept_manager.csv
+-- data/dept_manager.csv
 CREATE TABLE IF NOT EXISTS Dept_manager (
     dept_no CHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -42,15 +40,9 @@ CREATE TABLE IF NOT EXISTS Dept_manager (
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
 );
 
---salaries.csv
+-- data/salaries.csv
 CREATE TABLE IF NOT EXISTS Salaries (
     emp_no INT NOT NULL PRIMARY KEY,
     salary INT NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
 );
-
-
---Data Analysis
-
-
-
