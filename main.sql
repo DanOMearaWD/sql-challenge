75d6cb2b75889421dc79501a6b0b12f760814004
Dan O'Meara <danomearawd@gmail.com>
2024-10-17 01:16:39 -0400

    added comments

--- main.sql
+++ main.sql
@@ -1,13 +1,18 @@
-﻿CREATE TABLE IF NOT EXISTS Departments (
+﻿--Data Engineering
+
+--departments.csv
+CREATE TABLE IF NOT EXISTS Departments (
     dept_no CHAR(4) NOT NULL PRIMARY KEY,
     dept_name VARCHAR(25) NOT NULL
 );
 
+--titles.csv
 CREATE TABLE IF NOT EXISTS Titles (
     title_id CHAR(5) NOT NULL PRIMARY KEY,
     title VARCHAR(25) NOT NULL
 );
 
+--employees.csv
 CREATE TABLE IF NOT EXISTS Employees (
     emp_no INT NOT NULL PRIMARY KEY,
     emp_title_id CHAR(5) NOT NULL,
@@ -19,6 +24,7 @@ CREATE TABLE IF NOT EXISTS Employees (
     FOREIGN KEY (emp_title_id) REFERENCES Titles (title_id)
 );
 
+--dept_emp.csv
 CREATE TABLE IF NOT EXISTS Dept_emp (
     emp_no INT NOT NULL,
     dept_no CHAR(4) NOT NULL,
@@ -27,6 +33,7 @@ CREATE TABLE IF NOT EXISTS Dept_emp (
     FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
 );
 
+--dept_manager.csv
 CREATE TABLE IF NOT EXISTS Dept_manager (
     dept_no CHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -35,8 +42,15 @@ CREATE TABLE IF NOT EXISTS Dept_manager (
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
 );
 
+--salaries.csv
 CREATE TABLE IF NOT EXISTS Salaries (
     emp_no INT NOT NULL PRIMARY KEY,
     salary INT NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no)
 );
+
+
+--Data Analysis
+
+
+
